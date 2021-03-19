da3c7c26c5caff4d87d7d1d6210b5d81575dd977
leomuko <kezzieleo@gmail.com>
2021-03-19 07:24:04 +0300

    Create layout and modify code to display 3d model

---
+++ app/src/main/java/com/example/sceneexample/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.sceneexample;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment mArFragment;
+    private String Model_URL = "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf";
+    private ModelRenderable mModelRenderable;
+    private static final String TAG = "MainActivity";
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mArFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
+
+        mArFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                placeObject(mArFragment, anchor, Uri.parse(Model_URL));
+            }
+        });
+
+       // setUpModel();
+
+
+       //setContentView(R.layout.activity_ux);
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void placeObject(ArFragment arFragment, Anchor anchor, Uri uri) {
+        ModelRenderable.builder()
+                .setSource(arFragment.getContext(), RenderableSource.builder().setSource(this,
+                        uri,RenderableSource.SourceType.GLTF2 )
+                        .setScale(0.5f)
+                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                .build())
+                .setRegistryId(Model_URL)
+                .build()
+                .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
+                .exceptionally(throwable -> {
+                    Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                    return null;
+                });
+    }
+
+    private void addNodeToScene(ArFragment arFragment, Anchor anchor, ModelRenderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node= new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+
+    private boolean checkIsSupportedDeviceOrFinish(Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
